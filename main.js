c80b4c11b8690c82e1d23ab6946d2eb34e939922
Zein1-Waleed <140977773+Zein1-Waleed@users.noreply.github.com>
2023-12-29 13:12:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+   document.addEventListener('DOMContentLoaded',()=>{
+    // window.alert()
+    var audio = document.getElementById('backgroundAudio');
+
+      // Play the audio when the page is loaded
+      audio.play();
+
+
+      // Append the button to the body or a specific element
+    
+   })
